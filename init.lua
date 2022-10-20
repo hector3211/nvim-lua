b749dd55473a1d7388b2ae6d60e0c9a258eecfa7
hector3211 <hectororopesa5@gmail.com>
2022-10-20 16:40:26 -0400

    deleted old lsp stuff in init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.cmd("autocmd!")
 
-
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
@@ -13,7 +12,7 @@ vim.opt.si = true -- Smart indent
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.wrap = false -- No Wrap lines
-vim.opt.title =true
+vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.hlsearch = true
@@ -28,84 +27,48 @@ require('highlights')
 require('plugins')
 require('maps')
 
--- Typescript/ Javascript
--- require'lspconfig'.tsserver.setup {}
--- -- Tailwind
--- require'lspconfig'.tailwindcss.setup{}
--- -- CSS
--- require'lspconfig'.cssls.setup{}
--- --Astro
--- require'lspconfig'.astro.setup{}
--- -- Golang
--- require'lspconfig'.gopls.setup{}
--- -- RUST
--- require'lspconfig'.rust_analyzer.setup{}
--- -- Rust Tools
-local rt = require("rust-tools")
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
-
--- -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
---
--- -- The following example advertise capabilities to `clangd`.
--- require'lspconfig'.clangd.setup {
---   capabilities = capabilities,
--- }
--- -- Format on save
--- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
 
 -- Theme
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 
 -- Comments
 require('nvim_comment').setup()
 
--- Nvim Tree 
+-- Nvim Tree
 require("nvim-tree").setup()
 
 -- LuaLine
-require('lualine').setup()
+require('lualine').setup {}
 
 --BufferLine
-require("bufferline").setup{}
+require("bufferline").setup {}
 
 -- AutoPairs
 require('nvim-autopairs').setup({
-  disable_filetype = { "TelescopePrompt" , "vim" },
+    disable_filetype = { "TelescopePrompt", "vim" },
 })
 
 -- Prettier
 local prettier = require("prettier")
 prettier.setup({
-  bin = 'prettier', -- or `'prettierd'` (v0.22+)
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-    "go",
-    "rust"
-  },
+    bin = 'prettier', -- or `'prettierd'` (v0.22+)
+    filetypes = {
+        "css",
+        "graphql",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "less",
+        "markdown",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "yaml",
+    },
 })
 
+-- Astro Treesitter work around
 vim.filetype.add({
     extension = {
         astro = "astro"
