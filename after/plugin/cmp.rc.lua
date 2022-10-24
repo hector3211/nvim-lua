abee5b88af9276d4141ce92b23a424156012cb7d
hector3211 <hectororopesa5@gmail.com>
2022-10-24 14:19:42 -0400

    lsp change

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -15,7 +15,8 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<C-p'] = cmp.mapping.confirm({
+        ['<C-p>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
             select = true
         }),
     }),
